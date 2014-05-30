38116f49c30dddc4a6e6d4207a71f504c4cb9424
Karl Hardenstine <khardenstine@gmail.com>
2014-05-30 00:21:01 -0400

    Problem 1

---
+++ src/main/scala/euler/P001.scala
@@ -0,0 +1,15 @@
+package euler
+
+class P001 extends Problem {
+	def computeAnswer() = P001.sumOfMultiplesBelow(1000).toString
+}
+
+object P001 {
+	def sumOfMultiplesBelow(below: Int): Int = {
+		(0 until below).filter(isMultipleOf3or5).sum
+	}
+
+	def isMultipleOf3or5(number: Int): Boolean = {
+		number % 3 == 0 || number % 5 == 0
+	}
+}
---
+++ src/test/scala/euler/T001.scala
@@ -0,0 +1,17 @@
+package euler
+
+import org.scalatest.FunSuite
+
+class T001 extends FunSuite{
+	test("isMultipleOf3or5") {
+		assert(P001.isMultipleOf3or5(3))
+		assert(P001.isMultipleOf3or5(500))
+
+		assert(!P001.isMultipleOf3or5(1))
+		assert(!P001.isMultipleOf3or5(2))
+	}
+
+	test("sumOfMultiplesBelow 10 is 23") {
+		assert(P001.sumOfMultiplesBelow(10) == 23)
+	}
+}
