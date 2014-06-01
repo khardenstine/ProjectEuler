c79fabe08f08e50f5accfd99b39af335fd7da37d
Karl Hardenstine <khardenstine@gmail.com>
2014-06-01 19:44:59 -0400

    Problem 12

---
+++ src/test/scala/euler/T012.scala
@@ -0,0 +1,13 @@
+package euler
+
+import org.scalatest.FunSuite
+
+class T012 extends FunSuite {
+	test("the 7th triangle is 28") {
+		assert(P012.triangleNumber(6) == 28)
+	}
+
+	test("28 is the first triangle with over 5 divisors") {
+		assert(P012.findFirstTriangleNumberWithDivisors(5) == 28)
+	}
+}
