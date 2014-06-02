f2a8f340c9bcfd89a459d8eeade8d414a4de6b8b
Karl Hardenstine <khardenstine@gmail.com>
2014-06-01 20:28:55 -0400

    Problem 92

---
+++ src/test/scala/euler/T092.scala
@@ -0,0 +1,17 @@
+package euler
+
+import org.scalatest.FunSuite
+
+class T092 extends FunSuite {
+	test("sumOfSquaredDigits of 44 = 32") {
+		assert(P092.sumOfSquaredDigits(44) == 32)
+	}
+
+	test("chain starting with 44 does not end at 89") {
+		assert(!P092.chainEndsAt89(44))
+	}
+
+	test("chain starting with 145 ends at 89") {
+		assert(P092.chainEndsAt89(145))
+	}
+}
