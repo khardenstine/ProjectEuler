7d323e7e398bcaf0436f4c5916eeaa87fd763368
Karl Hardenstine <khardenstine@gmail.com>
2014-08-30 18:54:28 -0400

    Problem 26

---
+++ src/main/scala/euler/P026.scala
@@ -0,0 +1,85 @@
+package euler
+
+import P026._
+import scala.collection.mutable.ArrayBuffer
+
+class P026 extends Problem {
+  def computeAnswer() = {
+    getLargestRecurringCycleLessThan(1000)(DEFAULT_THRESHOLD).toString
+  }
+}
+
+object P026 {
+  final val DEFAULT_THRESHOLD = Threshold(10000, 10)
+
+  def getLargestRecurringCycleLessThan(d: Int)(implicit threshold: Threshold): Int = {
+    case class FractionCycle(d: Int, cycle: String) extends Ordered[FractionCycle] {
+      def compare(that: FractionCycle): Int = this.cycle.length.compare(that.cycle.length)
+    }
+
+    (2 to d)
+      .par
+      .flatMap(i => getUnitFractionRecurringCycle(i).map(FractionCycle(i, _)))
+      .max.d
+  }
+
+  def getUnitFractionRecurringCycle(d: Int)(implicit threshold: Threshold): Option[String] = {
+    assert(d > 1)
+
+    var digits = ArrayBuffer[Int]()
+
+    var dividend = 10
+    while(dividend != 0 && digits.size < threshold.length) {
+      digits += dividend / d
+      dividend = dividend % d * 10
+    }
+
+    getCycle(digits)
+  }
+  
+  def getCycle(digits: Seq[Int])(implicit threshold: Threshold): Option[String] = {
+    if (digits.size < threshold.length) {
+      None
+    } else {
+      val cycles = for (
+        startIdx <- (0 to threshold.length - threshold.repetitions).iterator;
+        cycleLength <- (1 to ((threshold.length - startIdx) / threshold.repetitions)).iterator
+      ) yield Cycle(startIdx, cycleLength, digits, threshold)
+
+      cycles.find(_.isValid).map(_.toString)
+    }
+  }
+
+  case class Cycle(start: Int, length: Int, digits: Seq[Int], threshold: Threshold) {
+    val slice = digits.slice(start, start + length)
+
+    def isValid: Boolean = {
+      var valid = true
+      var count = 0
+
+      var nextStart = start + length
+
+      while (valid && nextStart < digits.size && count < threshold.repetitions) {
+        val nextSlice = digits.slice(nextStart, nextStart + length)
+        val compareSlice = if (nextSlice.size == slice.size) slice else slice.take(nextSlice.size)
+        if (compareSlice != nextSlice) {
+          valid = false
+        }
+        count += 1
+        nextStart = nextStart + length
+      }
+      valid
+    }
+
+    override def toString: String = slice.mkString
+  }
+
+  /**
+   *
+   * @param length number of non-patterned digits to digest before qualifying as irrational
+   * @param repetitions minimum number of repetitions to find before qualifying as cyclic
+   */
+  case class Threshold(length: Int, repetitions: Int) {
+    assert(length > 0 && repetitions > 0)
+  }
+}
---
+++ src/test/scala/euler/T026.scala
@@ -0,0 +1,28 @@
+package euler
+
+import euler.P026._
+import org.scalatest.FunSuite
+
+class T026 extends FunSuite{
+  implicit val threshold = DEFAULT_THRESHOLD
+
+  test("the unit fraction of 2 has no recurring cycle") {
+    assert(getUnitFractionRecurringCycle(2).isEmpty)
+  }
+
+  test("the unit fraction of 3 has the recurring cycle of 3") {
+    assert(getUnitFractionRecurringCycle(3) == Some("3"))
+  }
+
+  test("the unit fraction of 6 has the recurring cycle of 6") {
+    assert(getUnitFractionRecurringCycle(6) == Some("6"))
+  }
+
+  test("the unit fraction of 7 has the recurring cycle of 142857") {
+    assert(getUnitFractionRecurringCycle(7) == Some("142857"))
+  }
+
+  test("the largest recurring cycle less than 11 is 7") {
+    assert(getLargestRecurringCycleLessThan(11) == 7)
+  }
+}
