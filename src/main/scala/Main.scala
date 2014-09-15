837a09a576849308f41bf33c3e3a1855282d20e5
Karl Hardenstine <khardenstine@gmail.com>
2014-09-14 23:14:23 -0400

    Add timings to Main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,16 +3,39 @@ import scala.util.{Success, Failure, Try}
 
 object Main {
 	def main(args: Array[String]): Unit = {
-		args.foreach{ arg =>
-			val message = reflect(arg) match {
-				case Success(problem) => s"$arg's answer is: " + problem.computeAnswer()
-				case Failure(e) => e.getMessage + "\n\t" + e.getStackTrace.map(_.toString).mkString("\n\t")
-			}
-			System.out.println(message)
+		args.foreach{
+      case "timings" => logTimings()
+      case arg => {
+        val message = reflect(arg) match {
+          case Success(problem) => {
+            val start = System.currentTimeMillis()
+            val answer = problem.computeAnswer()
+            val end = System.currentTimeMillis() - start
+            s"$arg's answer is: $answer\nComputed in " + end / 1000 + "." + end % 1000 + " seconds"
+          }
+          case Failure(e) => e.getMessage + "\n\t" + e.getStackTrace.map(_.toString).mkString("\n\t")
+        }
+        System.out.println(message)
+      }
 		}
 	}
 
 	def reflect(problemNumber: String): Try[Problem] = Try {
 		Class.forName("euler." + problemNumber).newInstance().asInstanceOf[Problem]
 	}
+
+  def logTimings() = {
+    (1 to 500)
+      .map(i => reflect("P%03d".format(i)))
+      .flatMap {
+        case Success(problem) => Some(problem)
+        case Failure(e) => None
+      }
+      .foreach(p => {
+        val start = System.currentTimeMillis()
+        p.computeAnswer()
+        val time: Long = System.currentTimeMillis() - start
+        println(p.getClass.getName + ": " + time / 1000 + "." + time % 1000 + " seconds")
+      })
+  }
 }
